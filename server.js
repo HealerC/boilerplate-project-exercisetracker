10da33291ebeab98b2f04a8e18793f1a6d74639e
Nosa-Ihaza Uyioghosa <ihazauyi@gmail.com>
2021-01-27 16:21:39 +0100

    Initial commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const mongoose = require('mongoose');
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,10 +10,31 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+  console.log("Connected to database");
+
+  const userExSchema = new mongoose.Schema({
+  	username: String,
+  	exerciseList: [{
+  		description: String,
+  		duration: Number,
+  		date: { type: Date, default: Date.now } 
+  	}]
+  });
+
+  const ExerciseTracker = mongoose.Model('ExerciseTracker', userExSchema);
+
+  
+  app.post('/api/exercise/new-user', )
+});
 
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
-})
+});
