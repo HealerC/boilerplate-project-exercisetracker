2067acd9b6f5fbf1a1645ad0caee3d964e32140d
Nosa-Ihaza Uyioghosa <ihazauyi@gmail.com>
2021-01-27 16:36:21 +0100

    Add route to create a new user

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-app.use(cors())
-app.use(express.static('public'))
+app.use(cors());
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/views/index.html');
 });
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
@@ -15,6 +16,7 @@ mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopolo
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 
+app.use(bodyParser.urlencoded({extended: false}));
 db.once('open', function() {
   console.log("Connected to database");
 
@@ -27,10 +29,40 @@ db.once('open', function() {
   	}]
   });
 
-  const ExerciseTracker = mongoose.Model('ExerciseTracker', userExSchema);
+  const ExerciseTracker = mongoose.model('ExerciseTracker', userExSchema);
 
-  
-  app.post('/api/exercise/new-user', )
+
+  app.post('/api/exercise/new-user', (req, res) => {
+  	ExerciseTracker.findOne({username: req.body.username}, (err, user) => {
+  		if (err) {
+  			console.error(err)
+  			res.send(err);
+  		}
+  		else if (!user) {
+  			const newUser = new ExerciseTracker({
+  				username: req.body.username,
+  				exerciseList: []
+  			});
+
+  			newUser.save((err, doc) => {
+  				if (err) {
+  					console.error(err);
+  					res.send(err);
+  				} else {
+  					res.json({
+  						username: doc.username,
+  						id: doc._id
+  					});
+  				}
+  			});
+  		} else {
+  			res.json({
+  				username: user.username,
+  				id: user._id
+  			});
+  		}
+  	});
+  });
 });
 
 
